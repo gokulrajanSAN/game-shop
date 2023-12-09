86294b09c3abe81e0931d2d1a99c8ce03e6eec3f
Jawahar Balasubramani <jawaharbjkk@gmail.com>
2023-12-09 01:59:13 -0500

    added theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import { Button, ThemeProvider, createTheme } from "@mui/material";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
 
 function App() {
-
+  const [mode, setMode] = useState("light");
+  const handleModeChange = () => setMode(mode === "light" ? "dark" : "light");
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <div>
+        <Navbar />
+        <Button
+          sx={{
+            bgcolor: "background.default",
+            color: "text.primary",
+            margin: "1rem",
+            "&:hover": { bgcolor: "blue" },
+          }}
+          onClick={handleModeChange}
+        >
+          Change Theme
+        </Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,44 @@
-import './Navbar.css'
+import { Box, Typography } from "@mui/material";
+import "./Navbar.css";
 
-const navItems = ["Products", "Cart", "Your Orders", "Login"]
+const navItems = ["Products", "Cart", "Your Orders", "Login"];
 
 const Navbar = () => {
-    return (
-        <div className='container'>
-            <div>
-                <img className='logo' src="src\assets\WhatsApp Image 2023-12-08 at 21.58.11.jpeg" alt="" />
-            </div>
-            <div className='search'></div>
-            {navItems.map(item => {
-                return (<div className='navItems' key={item} >{item}</div>)
-            })}
-            theme
-        </div>
-    )
-}
+  return (
+    <Box
+      className="container"
+      sx={{
+        bgcolor: "background.default",
+        color: "text.primary",
+        borderRadius: 1,
+        p: 3,
+      }}
+    >
+      <img
+        className="logo"
+        src="src\assets\WhatsApp Image 2023-12-08 at 21.58.11.jpeg"
+        alt=""
+      />
+      <Typography variant="h6" component="div" className="search">
+        Search
+      </Typography>
+      {navItems.map((item) => {
+        return (
+          <Typography
+            variant="h6"
+            component="div"
+            className="navItems"
+            key={item}
+          >
+            {item}
+          </Typography>
+        );
+      })}
+      <Typography variant="h6" component="div" className="navItems">
+        theme
+      </Typography>
+    </Box>
+  );
+};
 
-export default Navbar
+export default Navbar;
