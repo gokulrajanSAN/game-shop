cd05e4994d71cee69425c03cb9694f9cfcb0b8f4
Gokul-HP <gvarutharaj@gmail.com>
2023-12-12 08:49:21 +0530

    4th

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
     return (
         <div className='container'>
             <div>
-                <img className='logo' src="src\assets\WhatsApp Image 2023-12-08 at 21.58.11.jpeg" alt="" />
+                <img className='logo' src="src\assets\WhatsApp Image 2023-12-08 at 21.58.11.jpeg" alt="logo" />
             </div>
             <div className='search'></div>
             {navItems.map(item => {
                 return (<div className='navItems' key={item} >{item}</div>)
             })}
-            theme
+            <div>theme</div>
         </div>
     )
 }
